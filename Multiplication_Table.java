9defbe119f419ab193ca28eb19c1c244ba91e85e
ashfikur59 <mdashfikur23167@gmail.com>
2025-02-02 18:53:19 +0600

    Delete Multiplication_Table.java

--- Multiplication_Table.java
+++
@@ -1,9 +0,0 @@
-public class Multiplication_Table {
-    public static void main(String[] args) {
-        int i;
-        for(i=1;i<=10;i++)
-        {
-            System.out.println(25*i);
-        }
-    }
-}
